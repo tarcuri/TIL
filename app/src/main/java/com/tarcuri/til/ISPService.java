645858c15c22c73d295714ce7e4c03849ea42d20
tarcuri <thomas.j.arcuri@gmail.com>
2017-10-20 14:57:49 -0400

    started with basic usb connection code in ISPService

--- app/src/main/java/com/tarcuri/til/ISPService.java
+++ app/src/main/java/com/tarcuri/til/ISPService.java
@@ -1,17 +1,68 @@
 package com.tarcuri.til;
 
+import android.app.PendingIntent;
 import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.hardware.usb.UsbDevice;
+import android.hardware.usb.UsbDeviceConnection;
+import android.hardware.usb.UsbManager;
 import android.os.Binder;
+import android.os.Handler;
 import android.os.IBinder;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by tarcuri on 10/20/17.
  */
 
 public class ISPService extends Service {
+    private static final String ACTION_USB_ATTACHED = "android.hardware.usb.action.USB_DEVICE_ATTACHED";
+    private static final String ACTION_USB_DETACHED = "android.hardware.usb.action.USB_DEVICE_DETACHED";
+    private static final String ACTION_USB_PERMISSION = "com.android.example.USB_PERMISSION";
+    private static final int IPS_BAUD_RATE = 19200;
+    public static boolean SERVICE_CONNECTED = false;
 
     private final IBinder binder = new UsbBinder();
+
+    private Context context;
+    private Handler mHandler;
+    private UsbManager usbManager;
+    private UsbDevice usbDevice;
+    private UsbDeviceConnection usbDeviceConnection;
+    private boolean serialPortConnected;
+
+    private final BroadcastReceiver usbReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent.getAction().equals(ACTION_USB_PERMISSION)) {
+                boolean granted = intent.getExtras().getBoolean(UsbManager.EXTRA_PERMISSION_GRANTED);
+                if (granted) {
+                    // user granted permission
+                } else {
+                    // user did not grant permission
+                }
+            } else if (intent.getAction().equals(ACTION_USB_ATTACHED)) {
+
+            } else if (intent.getAction().equals(ACTION_USB_DETACHED)) {
+
+            }
+        }
+    };
+
+    @Override
+    public void onCreate() {
+        this.context = this;
+        serialPortConnected = false;
+        ISPService.SERVICE_CONNECTED = true;
+        setFilter();
+        usbManager = (UsbManager) getSystemService(Context.USB_SERVICE);
+        // findSerialPortDevice
+    }
 
     @Override
     public IBinder onBind(Intent intent) {
@@ -22,5 +73,52 @@ public class ISPService extends Service {
         public ISPService getService() {
             return ISPService.this;
         }
+    }
+
+    private void setFilter() {
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(ACTION_USB_PERMISSION);
+        filter.addAction(ACTION_USB_DETACHED);
+        filter.addAction(ACTION_USB_ATTACHED);
+        registerReceiver(usbReceiver, filter);
+    }
+
+    private void findSerialPortDevice() {
+        // This snippet will try to open the first encountered usb device connected, excluding usb root hubs
+        HashMap<String, UsbDevice> usbDevices = usbManager.getDeviceList();
+        if (!usbDevices.isEmpty()) {
+            boolean keep = true;
+            for (Map.Entry<String, UsbDevice> entry : usbDevices.entrySet()) {
+                usbDevice = entry.getValue();
+                int deviceVID = usbDevice.getVendorId();
+                int devicePID = usbDevice.getProductId();
+
+                if (deviceVID != 0x1d6b && (devicePID != 0x0001 && devicePID != 0x0002 && devicePID != 0x0003)) {
+                    // There is a device connected to our Android device. Try to open it as a Serial Port.
+                    requestUserPermission();
+                    keep = false;
+                } else {
+                    usbDeviceConnection = null;
+                    usbDevice = null;
+                }
+
+                if (!keep)
+                    break;
+            }
+            if (!keep) {
+                // There is no USB devices connected (but usb host were listed). Send an intent to MainActivity.
+//                Intent intent = new Intent(ACTION_NO_USB);
+//                sendBroadcast(intent);
+            }
+        } else {
+            // There is no USB devices connected. Send an intent to MainActivity
+//            Intent intent = new Intent(ACTION_NO_USB);
+//            sendBroadcast(intent);
+        }
+    }
+
+    private void requestUserPermission() {
+        PendingIntent mPendingIntent = PendingIntent.getBroadcast(this, 0, new Intent(ACTION_USB_PERMISSION), 0);
+        usbManager.requestPermission(usbDevice, mPendingIntent);
     }
 }
