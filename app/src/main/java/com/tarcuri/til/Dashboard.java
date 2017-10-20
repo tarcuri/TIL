798422aa096b8b95fef3e349d2f299a69edcccf2
tarcuri <thomas.j.arcuri@gmail.com>
2017-10-20 13:31:08 -0400

    Added class for Innovate Serial Protocol

--- app/src/main/java/com/tarcuri/til/Dashboard.java
+++ app/src/main/java/com/tarcuri/til/Dashboard.java
@@ -1,9 +1,14 @@
 package com.tarcuri.til;
 
 import android.graphics.Typeface;
+import android.hardware.usb.UsbDevice;
+import android.hardware.usb.UsbDeviceConnection;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+
+import com.felhr.usbserial.UsbSerialDevice;
 
 public class Dashboard extends AppCompatActivity {
 
@@ -12,6 +17,12 @@ public class Dashboard extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
 
+        TextView tv = (TextView) findViewById(R.id.afr_dashboard);
+        Typeface tf = Typeface.createFromAsset(getAssets(), "fonts/DSEG7Modern-Regular.ttf");
+        tv.setTypeface(tf);
+    }
+
+    public void connectUSB(View view) {
         TextView tv = (TextView) findViewById(R.id.afr_dashboard);
         Typeface tf = Typeface.createFromAsset(getAssets(), getString(R.string.afr_font));
         tv.setTypeface(tf);
---
+++ app/src/main/java/com/tarcuri/til/InnovateSerialProtocol.java
@@ -0,0 +1,8 @@
+package com.tarcuri.til;
+
+/**
+ * Created by tarcuri on 10/20/17.
+ */
+
+public class InnovateSerialProtocol {
+}
