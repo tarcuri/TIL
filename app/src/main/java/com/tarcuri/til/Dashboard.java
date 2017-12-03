c5fe95ab5ef32cdafc61f019032f97f0ae6adaeb
tarcuri <thomas.j.arcuri@gmail.com>
2017-12-02 21:59:40 -0500

    starting to add USB-serial manager to dashboard activity

--- app/src/main/java/com/tarcuri/til/Dashboard.java
+++ app/src/main/java/com/tarcuri/til/Dashboard.java
@@ -6,11 +6,47 @@ import android.content.Intent;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+import com.hoho.android.usbserial.driver.UsbSerialPort;
+import com.hoho.android.usbserial.util.SerialInputOutputManager;
 
 
 public class Dashboard extends AppCompatActivity {
+    private final String TAG = Dashboard.class.getSimpleName();
+
+    /**
+     * Driver instance, passed in statically via
+
+     * This is a devious hack; it'd be cleaner to re-create the driver using
+     * arguments passed in with the {@link #startActivity(Intent)} intent. We
+     * can get away with it because both activities will run in the same
+     * process, and this is a simple demo.
+     */
+    private static UsbSerialPort sPort = null;
+
+    private SerialInputOutputManager mSerialIoManager;
+
+    private final SerialInputOutputManager.Listener mListener =
+            new SerialInputOutputManager.Listener() {
+
+                @Override
+                public void onRunError(Exception e) {
+                    Log.d(TAG, "Runner stopped.");
+                }
+
+                @Override
+                public void onNewData(final byte[] data) {
+                    Dashboard.this.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Dashboard.this.updateReceivedData(data);
+                        }
+                    });
+                }
+            };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,8 +62,8 @@ public class Dashboard extends AppCompatActivity {
         log.append("starting ISPService...");
 
         // start the ISP service
-        Intent isp_intent = new Intent(this, ISPService.class);
-        startService(isp_intent);
+//        Intent isp_intent = new Intent(this, ISPService.class);
+//        startService(isp_intent);
 
         log.append("DONE.\n");
 
@@ -49,19 +85,43 @@ public class Dashboard extends AppCompatActivity {
         tv.setTypeface(tf);
     }
 
-    public void startLogging(View view) {
+//    // Our handler for received Intents. This will be called whenever an Intent
+//// with an action named "custom-event-name" is broadcasted.
+//    private BroadcastReceiver logReceiver = new BroadcastReceiver() {
+//        @Override
+//        public void onReceive(Context context, Intent intent) {
+//            // Get extra data included in the Intent
+//            String message = intent.getStringExtra("message");
+////            Log.d("receiver", "Got message: " + message);
+//
+//        }
+//    };
 
+    private void updateReceivedData(byte[] data) {
+//        final String message = "Read " + data.length + " bytes: \n"
+//                + HexDump.dumpHexString(data) + "\n\n";
+//        mDumpTextView.append(message);
+//        mScrollView.smoothScrollTo(0, mDumpTextView.getBottom());
     }
 
-    // Our handler for received Intents. This will be called whenever an Intent
-// with an action named "custom-event-name" is broadcasted.
-    private BroadcastReceiver logReceiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            // Get extra data included in the Intent
-            String message = intent.getStringExtra("message");
-//            Log.d("receiver", "Got message: " + message);
-
+    private void stopIoManager() {
+        if (mSerialIoManager != null) {
+            Log.i(TAG, "Stopping io manager ..");
+            mSerialIoManager.stop();
+            mSerialIoManager = null;
         }
-    };
+    }
+
+    private void startIoManager() {
+        if (sPort != null) {
+            Log.i(TAG, "Starting io manager ..");
+            mSerialIoManager = new SerialInputOutputManager(sPort, mListener);
+            //mExecutor.submit(mSerialIoManager);
+        }
+    }
+
+    private void onDeviceStateChange() {
+        stopIoManager();
+        startIoManager();
+    }
 }
