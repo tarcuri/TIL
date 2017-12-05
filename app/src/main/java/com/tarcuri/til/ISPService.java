afd34e6b84469407c583db91fcc7b9d3378778d5
tarcuri <thomas.j.arcuri@gmail.com>
2017-12-05 14:39:05 -0500

    refactored packet parsing code

--- app/src/main/java/com/tarcuri/til/ISPService.java
+++ app/src/main/java/com/tarcuri/til/ISPService.java
@@ -10,6 +10,7 @@ import android.os.IBinder;
 import android.util.Log;
 
 import com.hoho.android.usbserial.driver.UsbSerialPort;
+import com.hoho.android.usbserial.util.HexDump;
 
 import java.io.IOException;
 import java.nio.ByteBuffer;
@@ -85,80 +86,103 @@ public class ISPService extends Service {
                     if (br == 0) {
                         Log.d(TAG, "read timed out");
                         continue;
+                    } else {
+                        Log.d(TAG, "read " + br + "bytes: " + HexDump.dumpHexString(bbuf) + "\n");
+                        // chunk access for debug
+                        mChunkQueue.add(bbuf);
+                        sendBroadcast(new Intent(ISPService.ISP_DATA_RECEIVED));
                     }
 
-                    // chunk access for debug
-                    mChunkQueue.add(bbuf);
-                    sendBroadcast(new Intent(ISPService.ISP_DATA_RECEIVED));
+                    // now process the bytebuffer
+                    mByteBuffer.put(bbuf);
 
-//                    mByteBuffer.put(bbuf);
-//
-//                    // mark this location to continue reading
-//                    mByteBuffer.mark();
-//
-//                    // now flip for reading
-//                    mByteBuffer.limit(mByteBuffer.position());
-//                    mByteBuffer.position(read_mark);
-//
-//                    // convert to short buffer to check for packets
-//                    int limit = mByteBuffer.limit() - mByteBuffer.position(); // num bytes in buffer
-//                    int num_words = limit / 2;
-//                    short[] words = new short[num_words];
-//                    mByteBuffer.order(ByteOrder.BIG_ENDIAN).asShortBuffer().get(words);
-//
-//                    // check for valid packets
-//                    int packet_words_total = 0;
-//                    int packet_words_read = 0;
-//                    short[] packet = null;
-//                    for (short word : words) {
-//                        if ((word & ISP_HEADER_MASK) == ISP_HEADER_MASK) {
-//                            // found a header, get the packet length (in words)
-//                            packet_words_total = (byte) (word & ISP_LOW_LENGTH_MASK);
-//                            if ((word & ISP_HIGH_BIT_LENGTH_MASK) != 0) {
-//                                packet_words_total |= (1 << 7);
-//                            }
-//                            packet = new short[packet_words_total + 1];
-//                            packet[0] = word;
-//                        } else if (packet != null) {
-//                            if (packet_words_read < packet_words_total) {
-//                                packet[++packet_words_read] = word;
-//                            }
-//                        } else {
-//                            Log.i(TAG, "discarding word without preceding header");
-//
-//                            // consume this word
-//                            mByteBuffer.order(ByteOrder.BIG_ENDIAN).getShort();
-//                        }
-//                    }
-//
-//                    // consume data for valid packets
-//                    if (packet_words_read == packet_words_total) {
-//                        // consume words for this packet
-//                        for (int i = 0; i < packet_words_read; i++) {
-//                            mByteBuffer.order(ByteOrder.BIG_ENDIAN).getShort();
-//                        }
-//
-//                        Log.d(TAG, "received LC1 packet");
-//                        mPacketQueue.add(new LC1Packet(packet));
-//                        sendBroadcast(new Intent(ISPService.ISP_LC1_RECEIVED));
-//                    }
-//
-//                    read_mark = mByteBuffer.position();
-//
-//                    // did we read up to the mark?
-//                    if (mByteBuffer.hasRemaining()) {
-//                        // more to read to reset limit to capacity
-//                        mByteBuffer.limit(mByteBuffer.capacity());
-//                    }
-//
-//                    // now reset and check for remaining data
-//                    mByteBuffer.reset();
-//                    if (!mByteBuffer.hasRemaining()) {
-//                        // if no remaining unconsumed bytes then clear the buffer
-//                        // NOTE: this will potentially discard fragments of a packet
-//                        mByteBuffer.clear();
-//                        read_mark = mByteBuffer.position();
-//                    }
+                    // mark this location to continue reading
+                    mByteBuffer.mark();
+
+                    // now flip for reading
+                    mByteBuffer.limit(mByteBuffer.position());
+                    mByteBuffer.position(read_mark);
+
+                    // convert to short buffer to check for packets
+                    int limit = mByteBuffer.limit() - mByteBuffer.position(); // num bytes in buffer
+                    int num_words = limit / 2;
+                    if (num_words == 0) {
+                        Log.d(TAG, "no words available (" + br + " bytes)");
+                        // need to get more bytes, set limit to capacity
+                        mByteBuffer.limit(mByteBuffer.capacity());
+                        continue;
+                    }
+
+                    Log.d(TAG, "checking " + num_words + " words for packets");
+
+                    // now see if we can find a packet in the bytebuffer
+                    short[] words = new short[num_words];
+                    mByteBuffer.order(ByteOrder.BIG_ENDIAN).asShortBuffer().get(words);
+
+                    // packet headers include packet length (in words)
+                    int packet_words_total = 0;
+                    int packet_words_read = 0;
+                    int words_consumed = 0;
+                    short[] packet = null;
+                    for (short word : words) {
+                        if ((word & ISP_HEADER_MASK) == ISP_HEADER_MASK) {
+                            // found a header, get the packet length (in words)
+                            packet_words_total = (byte) (word & ISP_LOW_LENGTH_MASK);
+                            if ((word & ISP_HIGH_BIT_LENGTH_MASK) != 0) {
+                                packet_words_total |= (1 << 7);
+                            }
+                            Log.d(TAG, "found header, len = " + packet_words_total);
+                            packet = new short[packet_words_total + 1];
+                            packet_words_read = 0;
+                            packet[0] = word;
+
+                            words_consumed++;
+                        } else if (packet != null) {
+                            if (packet_words_read < packet_words_total) {
+                                packet[++packet_words_read] = word;
+
+                                words_consumed++;
+                                Log.d(TAG, "appended word to packet ["+ packet_words_read + "/" + packet_words_total + "]");
+                            }
+
+                            if (packet_words_read == packet_words_total) {
+                                Log.d(TAG, "queueing LC1 packet");
+                                mPacketQueue.add(new LC1Packet(packet));
+                                sendBroadcast(new Intent(ISPService.ISP_LC1_RECEIVED));
+
+                                packet = null;
+                                packet_words_read = 0;
+                                packet_words_total = 0;
+
+                                // increment position to consume bytes
+                                mByteBuffer.position(mByteBuffer.position() + words_consumed*2);
+                            }
+                        } else {
+                            Log.d(TAG, "ignoring non-header word");
+
+                            words_consumed++;
+                        }
+                    }
+
+                    read_mark = mByteBuffer.position();
+
+                    // did we read up to the mark?
+                    if (mByteBuffer.hasRemaining()) {
+                        // more to read to reset limit to capacity
+                        Log.d(TAG, "did not consume all available bytes");
+                        mByteBuffer.limit(mByteBuffer.capacity());
+                    } else {
+                        Log.d(TAG, "consumed all available bytes");
+                    }
+
+                    // now reset and check for remaining data
+                    mByteBuffer.reset();
+                    if (!mByteBuffer.hasRemaining()) {
+                        // if no remaining unconsumed bytes then clear the buffer
+                        // NOTE: this will potentially discard fragments of a packet
+                        mByteBuffer.clear();
+                        read_mark = mByteBuffer.position();
+                    }
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
