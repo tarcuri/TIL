53e02c2b50cd1fbeedf60be05f243c7e4a6d15bc
tarcuri <thomas.j.arcuri@gmail.com>
2017-12-05 20:37:37 -0500

    refactored bytebuffer code, again. now consumes immediately and clears at the end of the loop body

--- app/src/main/java/com/tarcuri/til/ISPService.java
+++ app/src/main/java/com/tarcuri/til/ISPService.java
@@ -59,6 +59,8 @@ public class ISPService extends Service {
     public static final String ISP_DATA_RECEIVED = "com.tarcuri.til.ISP_DATA_RECEIVED";
     public static final String ISP_LC1_RECEIVED = "com.tarcuri.til.ISP_LC1_RECEIVED";
 
+    // LC1 length in words (not including 1 word header)
+    private static int ISP_LC1_PACKET_LENGTH = 2;
     // bits 15, 13, 9, and 7 are always 1 in header
     private static int ISP_HEADER_MASK = 0xa280;
     private static short ISP_HIGH_BIT_LENGTH_MASK = 0x100;
@@ -76,6 +78,9 @@ public class ISPService extends Service {
                 int read_mark = 0;
                 byte[] chunk;
                 byte[] bbuf = new byte[32];
+                short[] packet = null;
+                int packet_words = 0;
+                int words_read = 0;
 
                 mByteBuffer.clear();
 
@@ -100,94 +105,61 @@ public class ISPService extends Service {
                     // now process the bytebuffer
                     mByteBuffer.put(chunk);
 
-                    // mark this location to continue reading
-                    mByteBuffer.mark();
-
-                    // now flip for reading
-                    mByteBuffer.limit(mByteBuffer.position());
-                    mByteBuffer.position(read_mark);
-
-                    // convert to short buffer to check for packets
-                    int limit = mByteBuffer.limit() - mByteBuffer.position(); // num bytes in buffer
-                    int num_words = limit / 2;
-                    if (num_words == 0) {
-                        Log.d(TAG, "no words available (" + br + " bytes)");
-                        // need to get more bytes, set limit to capacity
-                        mByteBuffer.reset();
-                        mByteBuffer.limit(mByteBuffer.capacity());
+                    if (mByteBuffer.position() == 1) {
+                        // only have 1 byte, no sense in going further
+                        // just continue to read
+                        Log.d(TAG, "only read 1 byte - continuing");
                         continue;
                     }
 
+                    mByteBuffer.flip();
+
+                    int len = mByteBuffer.limit() - mByteBuffer.position();
+                    int num_words = len / 2;
+
                     Log.d(TAG, "checking " + num_words + " words for packets");
 
-                    // now see if we can find a packet in the bytebuffer
-                    short[] words = new short[num_words];
-                    mByteBuffer.order(ByteOrder.BIG_ENDIAN).asShortBuffer().get(words);
-
-                    // packet headers include packet length (in words)
-                    int packet_words_total = 0;
-                    int packet_words_read = 0;
-                    int words_consumed = 0;
-                    short[] packet = null;
-                    for (short word : words) {
-                        if ((word & ISP_HEADER_MASK) == ISP_HEADER_MASK) {
-                            // found a header, get the packet length (in words)
-                            packet_words_total = (byte) (word & ISP_LOW_LENGTH_MASK);
+                    // packet length in words, not including header
+                    for (int i = 0; i < num_words; i++) {
+                        short word = mByteBuffer.getShort();
+                        if ((word & ISP_HEADER_MASK) == 1) {
+                            packet_words = (byte) (word & ISP_LOW_LENGTH_MASK);
                             if ((word & ISP_HIGH_BIT_LENGTH_MASK) != 0) {
-                                packet_words_total |= (1 << 7);
+                                packet_words |= 1 << 7;
                             }
-                            Log.d(TAG, "found header, len = " + packet_words_total);
-                            packet = new short[packet_words_total + 1];
-                            packet_words_read = 0;
+                            Log.d(TAG, "found header, len = " + packet_words);
+                            packet = new short[packet_words + 1];
                             packet[0] = word;
-
-                            words_consumed++;
+                            words_read = 0;
                         } else if (packet != null) {
-                            if (packet_words_read < packet_words_total) {
-                                packet[++packet_words_read] = word;
-
-                                words_consumed++;
-                                Log.d(TAG, "appended word to packet ["+ packet_words_read + "/" + packet_words_total + "]");
+                            if (words_read < packet_words) {
+                                packet[++words_read] = word;
+                                Log.d(TAG, "appended word to packet ["+ words_read + "/" + packet_words + "]");
                             }
 
-                            if (packet_words_read == packet_words_total) {
-                                Log.d(TAG, "queueing LC1 packet");
-                                mPacketQueue.add(new LC1Packet(packet));
-                                sendBroadcast(new Intent(ISPService.ISP_LC1_RECEIVED));
+                            if (words_read == packet_words) {
+                                Log.d(TAG, "found full packet (" + packet_words + " words)");
+                                if (packet_words == ISP_LC1_PACKET_LENGTH) {
+                                    Log.d(TAG, "found LC1 packet");
+                                    mPacketQueue.add(new LC1Packet(packet));
+                                    sendBroadcast(new Intent(ISPService.ISP_LC1_RECEIVED));
+                                }
 
                                 packet = null;
-                                packet_words_read = 0;
-                                packet_words_total = 0;
-
-                                // increment position to consume bytes
-                                mByteBuffer.position(mByteBuffer.position() + words_consumed*2);
+                                packet_words = 0;
+                                words_read = 0;
                             }
                         } else {
                             Log.d(TAG, "ignoring non-header word");
-
-                            words_consumed++;
                         }
                     }
 
-                    read_mark = mByteBuffer.position();
-
-                    // did we read up to the mark?
-                    if (mByteBuffer.hasRemaining()) {
-                        // more to read to reset limit to capacity
-                        Log.d(TAG, "did not consume all available bytes");
-                        mByteBuffer.limit(mByteBuffer.capacity());
-                    } else {
-                        Log.d(TAG, "consumed all available bytes");
+                    if (mByteBuffer.limit() - mByteBuffer.position() == 1) {
+                        // only 1 byte left
+                        Log.d(TAG, "1 byte left in ByteBuffer - discarding");
                     }
 
-                    // now reset and check for remaining data
-                    mByteBuffer.reset();
-                    if (!mByteBuffer.hasRemaining()) {
-                        // if no remaining unconsumed bytes then clear the buffer
-                        // NOTE: this will potentially discard fragments of a packet
-                        mByteBuffer.clear();
-                        read_mark = mByteBuffer.position();
-                    }
+                    mByteBuffer.clear();
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
